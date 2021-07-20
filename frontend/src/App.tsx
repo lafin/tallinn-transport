ba4d9ad48fdc55569e9998b669c2096a59746178
Sergei Kuvakin <sergey.kuvakin@coolbet.com>
2021-07-20 22:06:20 +0300

    feat: replaced react-leaflet to native

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { Map, TileLayer, CircleMarker } from "react-leaflet";
+import React, { useEffect, useState, useRef } from "react";
 import { preparePointsToMap, getData } from "./providers";
 import { Point } from "./interfaces";
 import "./App.css";
+import {
+  TileLayer,
+  Map as LeafletMap,
+  CircleMarker,
+  LayerGroup,
+} from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 const App: React.FunctionComponent = () => {
-  const [init] = useState({
-    lat: 59.436962,
-    lng: 24.753574,
-    zoom: 13,
-  });
+  const mapRef = useRef<HTMLDivElement>(null);
+  const [markers, setMarkers] = useState<LayerGroup | null>(null);
+
   const [points, setPoints] = useState<Point[]>([]);
 
   useEffect(() => {
@@ -35,28 +38,46 @@ const App: React.FunctionComponent = () => {
     }
   }
 
-  return (
-    <Map center={[init.lat, init.lng]} zoom={init.zoom}>
-      <TileLayer
-        attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
-        url="https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}"
-        id="mapbox/light-v10"
-        accessToken="pk.eyJ1IjoibGFmaW4iLCJhIjoiY2tjZ203MXljMGM2ajJ5cWszMG1wY2ExdCJ9.cPdi63ptWIqYt6MRLMseVg"
-      />
-      <>
-        {points.map((point) => (
-          <CircleMarker
-            key={`marker-${point.vehicleType}-${point.vehicleNumber}`}
-            center={[point.latitude, point.longitude]}
-            radius={3}
-            fill={true}
-            weight={1}
-            color={getColor(point.vehicleType)}
-          />
-        ))}
-      </>
-    </Map>
-  );
+  useEffect(() => {
+    if (mapRef.current) {
+      const map = new LeafletMap(mapRef.current);
+      map.setView([59.436962, 24.753574], 13);
+      const tileLayer = new TileLayer(
+        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+        {
+          attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          id: "mapbox/light-v10",
+          accessToken:
+            "pk.eyJ1IjoibGFmaW4iLCJhIjoiY2tjZ203MXljMGM2ajJ5cWszMG1wY2ExdCJ9.cPdi63ptWIqYt6MRLMseVg",
+        }
+      );
+      map.addLayer(tileLayer);
+      const markersLayer = new LayerGroup();
+      map.addLayer(markersLayer);
+      setMarkers(markersLayer);
+    }
+  }, [mapRef]);
+
+  useEffect(() => {
+    if (markers) {
+      markers.eachLayer((marker) => marker.remove());
+      for (const point of points) {
+        const circleMarker = new CircleMarker(
+          [point.latitude, point.longitude],
+          {
+            radius: 3,
+            fill: true,
+            weight: 1,
+            color: getColor(point.vehicleType),
+          }
+        );
+        markers.addLayer(circleMarker);
+      }
+    }
+  }, [points, markers]);
+
+  return <div ref={mapRef} />;
 };
 
 export default App;
